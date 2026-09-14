c30e69eb8525dd6813c20cc736720edd5f608917
Kurt7275 <kbautista99931@liceo.edu.ph>
2026-09-15 00:33:34 +0800

    fix: dynamically construct google redirect URL from APP_URL

--- backend/config/services.php
+++ backend/config/services.php
@@ -38,7 +38,9 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => (env('GOOGLE_REDIRECT_URI') && parse_url(env('GOOGLE_REDIRECT_URI'), PHP_URL_HOST) && !preg_match('/[\$\{\}\[\]]/', (string) parse_url(env('GOOGLE_REDIRECT_URI'), PHP_URL_HOST))) ? env('GOOGLE_REDIRECT_URI') : 'http://localhost/api/auth/google/callback',
+        'redirect' => (env('GOOGLE_REDIRECT_URI') && parse_url(env('GOOGLE_REDIRECT_URI'), PHP_URL_HOST) && !preg_match('/[\$\{\}\[\]]/', (string) parse_url(env('GOOGLE_REDIRECT_URI'), PHP_URL_HOST)))
+            ? env('GOOGLE_REDIRECT_URI')
+            : rtrim((env('APP_URL') && parse_url(env('APP_URL'), PHP_URL_HOST) && !preg_match('/[\$\{\}\[\]]/', (string) parse_url(env('APP_URL'), PHP_URL_HOST))) ? env('APP_URL') : 'http://localhost', '/').'/api/auth/google/callback',
     ],
 
 ];
